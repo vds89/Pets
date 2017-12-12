d2fd3456b532dc54f626fcc5acb493c02d4e4ab4
Vincenzo De Simone <vincenzodesimone04@gmail.com>
2017-12-12 22:10:45 +0100

    PetProvider class & URI construction in PetContract class

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -32,6 +33,34 @@ public final class PetContract {
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
 
+        /** Complete CONTENT_URI - inside each of the Entry classes in the contract,
+         * we create a full URI for the class as a constant called CONTENT_URI.
+         * The Uri.withAppendedPath() method appends the BASE_CONTENT_URI (which contains the scheme
+         * and the content authority) to the path segment. */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
     }
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+     /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
 
 }
--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -16,10 +16,12 @@ public class PetDbHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "shelter.db";
 
+    //Class constructor
     public PetDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    //SQLite TABLE creation
     private static final String SQL_CREATE_PETS_TABLE =
             "CREATE TABLE " + PetContract.PetEntry.TABLE_NAME + " (" +
                     PetContract.PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -28,6 +30,7 @@ public class PetDbHelper extends SQLiteOpenHelper {
                     PetContract.PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, " +
                     PetContract.PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0)";
 
+    //SQLite TABLE deleting
     private static final String SQL_DELETE_PETS_TABLE =
             "DROP TABLE IF EXISTS " + PetContract.PetEntry.TABLE_NAME;
 
